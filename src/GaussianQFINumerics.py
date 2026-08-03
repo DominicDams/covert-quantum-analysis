d94db0a4cf202c07212a2f065f83de5566bafcdc
Dominic Dams <q2dominic@gmail.com>
2026-08-03 14:13:37 -0700

    Updated Gaussian numerics to work on all sizes of covariance matrix

--- src/GaussianQFINumerics.py
+++ src/GaussianQFINumerics.py
@@ -4,65 +4,76 @@ import numpy as np
 import scipy as sp
 from tqdm.autonotebook import tqdm
 
-Ω = np.array([
-        [0,1,0,0,0,0],
-        [-1,0,0,0,0,0],
-        [0,0,0,1,0,0],
-        [0,0,-1,0,0,0],
-        [0,0,0,0,0,1],
-        [0,0,0,0,-1,0]
+Omega1 = np.array([
+        [0,1],
+        [-1,0]
 ])
-def will_decomp(σ):
+def Omega(n):
+    """Gives the n mode symplectic form
+    Parameters
+    ----------
+    n: np.double
+        The number of modes
+    Returns
+    -------
+    np.double[2*n,2*n]
+        The n mode symplectic form
+    """
+    return sp.linalg.block_diag(*([Omega1]*n))
+def will_decomp(sigma):
     """Calculates the Williamson Decomposition of a symmetric matrix
     Parameters
     ----------
-    sigma: np.cdouble[6,6]
+    sigma: np.cdouble[2n,2n]
         The symmetric matrix matrix
     Returns
     -------
-    [np.cdouble[6,6],np.cdouble[3]]
-        S and v s.t. S@diag(v[0],v[0],v[1],v[1],v[2],v[2])@transpose(S) = sigma
+    [np.cdouble[2n,2n],np.cdouble[n]]
+        S and nu s.t. S@diag(*nu)@transpose(S) = sigma
     """
-    # Currently built for 6x6 covariance matricies, but could be adapted for other matricies
-    sqrt = sp.linalg.sqrtm(σ)
+    n = int(len(sigma)/2)
+    Omegaval = Omega(n)
+    sqrt = sp.linalg.sqrtm(sigma)
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message="invalid value encountered in divide")
         warnings.filterwarnings("ignore", message="divide by zero encountered in divide")
         warnings.filterwarnings("ignore", message="divide by zero encountered in reciprocal")
         # May be faster to invert the square-rooted matrix, though more ideally we could batch these
-        #invsqrt = sp.linalg.fractional_matrix_power(σ,-.5)
+        #invsqrt = sp.linalg.fractional_matrix_power(sigma,-.5)
         invsqrt = np.linalg.inv(sqrt)
-        Ψ = invsqrt@Ω@invsqrt
+        Psi = invsqrt@Omegaval@invsqrt
     #[T,Z,_] = sp.linalg.schur(Ψ,output='real',sort='lhp')
-        [T,Z] = sp.linalg.schur(Ψ,output='real')
+        [T,Z] = sp.linalg.schur(Psi,output='real')
         I2 = np.array([[1,0],[0,1]])
         Px = np.array([[0,1],[1,0]])
-        Πl = [I2 if T[2*i,2*i+1] >0 else Px for i in range(3)]
-        Π = sp.linalg.block_diag(Πl[0],Πl[1],Πl[2])
-        Dinv = np.abs(np.linalg.diagonal(T@Ω))
+        Pil = [I2 if T[2*i,2*i+1] >0 else Px for i in range(n)]
+        Pi = sp.linalg.block_diag(*Pil)
+        Dinv = np.abs(np.linalg.diagonal(T@Omegaval))
         Dinvsqrt = np.sqrt(Dinv)
         #Dt =  np.reciprocal(Dinv)
-        St = sqrt@Z@Π@np.diag(Dinvsqrt)
+        St = sqrt@Z@Pi@np.diag(Dinvsqrt)
         # Correct numerical issues, these were initially caused by a typo,so they shouldn't be present anymore
         #err = -St@Ω@np.transpose(St)@Ω
         #print(err)
         #fix = sp.linalg.fractional_matrix_power(err,-.5)
     S = St#fix@St
-    Sinv = -Ω@np.transpose(S)@Ω
+    Sinv = -Omegaval@np.transpose(S)@Omegaval
     SinvT = np.transpose(Sinv)
-    ν = [(Sinv@σ@SinvT)[2*i][2*i] for i in range(3)]
+    nu = [(Sinv@sigma@SinvT)[2*i][2*i] for i in range(n)]
     # D = np.linalg.diagonal(err@np.diag(Dt))
-    #print(St@np.diag(Dt)@np.transpose(St)-σ)
+    #print(St@np.diag(Dt)@np.transpose(St)-sigma)
     #print(np.round(St@Ω@np.transpose(St),5))
     #print(Dt)
     #print(np.round(S@Ω@np.transpose(S),8))
-    return [S,ν]
-def _SLD_precomp(σ):
-    [S,ν] = will_decomp(σ)
-    Sinv = -Ω@np.transpose(S)@Ω
+    return [S,nu]
+def _SLD_precomp(sigma):
+    [S,ν] = will_decomp(sigma)
+    n = int(len(sigma)/2)
+    Omegaval = Omega(n)
+    Sinv = -Omegaval@np.transpose(S)@Omegaval
     SinvT = np.transpose(Sinv)
     def M(j,k,m):
-        Mval = np.zeros([6,6])
+        Mval = np.zeros([2*n,2*n])
         outputs = {
             0: [[0,1],[-1,0]],
             1: [[0,1],[1,0]],
@@ -71,18 +82,18 @@ def _SLD_precomp(σ):
         }
         Mval[(2*j):(2*j+2),(2*k):(2*k+2)] = outputs[m]
         return Mval
-    newM = [[[SinvT@M(j,k,m)@Sinv for m in range(3)] for k in range(3)] for j in range(3)]
+    newM = [[[SinvT@M(j,k,m)@Sinv for m in range(n)] for k in range(n)] for j in range(n)]
     return [newM,ν]
     
-def SLD(σ,dσ,precomp = None):
+def SLD(sigma,dσ,precomp = None):
     """Calculates the Symmetric Logarithmic Derivative
     Parameters
     ----------
-    sigma: np.cdouble[6,6]
+    sigma: np.cdouble[2m,2m]
         The covariance matrix
-    dsigma: np.cdouble[n,6,6]
+    dsigma: np.cdouble[n,2m,2m]
         Derivative of the covariance matrix
-    precomp: [np.cdouble[6,6][3,3],np.cdouble[3]]
+    precomp: [np.cdouble[2m,2m][m,m],np.cdouble[m]]
         A precomputation step useful in the case where we want to calculate SLDs for multiple parameters
     Returns
     -------
@@ -90,13 +101,14 @@ def SLD(σ,dσ,precomp = None):
         The Symmetric Logarithmic Derivative
     """
     if precomp is None:
-        precomp = _SLD_precomp(σ)
+        precomp = _SLD_precomp(sigma)
     newM = precomp[0]
+    m = int(len(sigma)/2)
     ν=precomp[1]
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message="invalid value encountered in divide")
         warnings.filterwarnings("ignore", message="divide by zero encountered in divide")
-        Lsum = [[[ newM[i][j][k] * 1/(ν[i]*ν[j] - (-1)**k)*np.linalg.trace(newM[i][j][k]@dσ) for i in range(3)] for j in range(3)] for k in range(3)]
+        Lsum = [[[ newM[i][j][k] * 1/(ν[i]*ν[j] - (-1)**k)*np.linalg.trace(newM[i][j][k]@dσ) for i in range(m)] for j in range(m)] for k in range(m)]
     L = np.nansum(Lsum,axis=(0,1,2))
     return L
     #print(ν)
@@ -105,13 +117,13 @@ def FIM(σ,dσs):
     """Calculates the Quantum Fisher Information Matrix
     Parameters
     ----------
-    sigma: np.cdouble[6,6]
+    sigma: np.cdouble[2m,2m]
         The covariance matrix
-    dsigmas: np.cdouble[n,6,6]
+    dsigmas: np.cdouble[n,2m,2m]
         An array of derivatives of the covariance matrix
     Returns
     -------
-    np.cdouble[len(dsigmas),len(dsigmas)]
+    np.cdouble[n,n]
         The Quantum Fisher Information matrix
     """
     precomp = _SLD_precomp(σ)
