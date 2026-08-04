15260bddddbb5bd84217958465aaae06fc0f5386
Dominic Dams <q2dominic@gmail.com>
2026-08-04 14:07:15 -0700

    Updated code to use library style build and import in uv

---
+++ src/covert_quantum_analysis/__init__.py
@@ -0,0 +1,11 @@
+import qutip
+
+from GaussianQFINumerics import FIM as FIM
+from GaussianQFINumerics import SLD as SLD
+from GaussianQFINumerics import vectorized_QFIM as vectorized_QFIM
+from QFINumerics import QFI as QFI
+from QFINumerics import FI_observable as FI_observable
+from QFINumerics import calc_for_state as calc_for_state
+from QFINumerics import genChannel as genChannel
+
+del qutip
